25da49192706e1c112302bdd6cd8ef1cdee7ab4c
akhil panwar <akhilpanwar180@gmail.com>
2022-10-28 17:47:55 +0530

    created get and post routes for places

---
+++ server/Controllers/places-controllers.js
@@ -0,0 +1,91 @@
+import HttpError from "../Models/http-error.js";
+import { nanoid } from "nanoid";
+
+const PLACES = [
+  {
+    id: "p1",
+    imageUrl:
+      "https://s3.amazonaws.com/images.skyscrapercenter.com/thumbs/27711_500x650.jpg",
+    title: "Empire State building p1",
+    description: "famous sky scraper",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    creator: "u1",
+    location: {
+      lat: "40.7483565",
+      lng: "-73.9878531",
+    },
+  },
+  {
+    id: "p2",
+    imageUrl:
+      "https://s3.amazonaws.com/images.skyscrapercenter.com/thumbs/27711_500x650.jpg",
+    title: "Empire State building p2",
+    description: "famous sky scraper",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    creator: "u2",
+    location: {
+      lat: "40.7483565",
+      lng: "-73.9878531",
+    },
+  },
+];
+
+export const getPlacesByPlaceId = (req, res, next) => {
+  const { pid } = req.params;
+  const place = PLACES.find((place) => place.id === pid);
+  if (!place) {
+    throw new HttpError("Could not find the place for provided place id", 404);
+  } else {
+    res.json({ place });
+  }
+};
+
+export const getPlacesByUserId = (req, res, next) => {
+  const { uid } = req.params;
+  const place = PLACES.find((place) => place.creator === uid);
+  if (!place) {
+    const error = new HttpError(
+      "Could not find the place for provided user id.",
+      404
+    );
+    next(error);
+  } else {
+    res.json({ place });
+  }
+};
+
+export const createPlace = (req, res, next) => {
+  const { title, description, address, creator, coordinates } = req.body;
+  const createdPlace = {
+    id: nanoid(),
+    title,
+    description,
+    address,
+    creator,
+    location: coordinates,
+  };
+  console.log(createdPlace);
+  PLACES.push(createdPlace);
+  console.log(PLACES);
+  res.status(201).json({ createdPlace });
+};
+
+export const updatePlace = (req, res, next) => {
+  const { pid } = req.params;
+  const { title, description } = req.body;
+
+  const updatedPlace = {...(PLACES.find((place) => place.id === pid))};
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+  const placeIndex = PLACES.findIndex((p) => p.id === pid);
+  console.log(placeIndex);
+  PLACES[placeIndex] = updatePlace;
+
+  res.status(200).json({ updatedPlace });
+};
+
+console.log(PLACES);
+
+export const deletePlace = (req, res, next) => {
+  const { pid } = req.params;
+};
--- server/index.js
+++ server/index.js
@@ -4,15 +4,28 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import HttpError from "./Models/http-error.js";
 import placesRoutes from "./Routes/places-routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/places", placesRoutes);
 
+app.use((req, res, next) => { //in case the route does not match any of the above
+  throw new HttpError("Could not find the route.", 404);
+});
+
+app.use((error, req, res, next) => {
+  //middleware for error handling for all requests.
+  if (res.headerSent) {
+    next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "An unknown error occurred!" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is listening at port ${process.env.PORT}`);
