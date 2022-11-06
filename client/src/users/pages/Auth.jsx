d8e64b1a1b92569368031f8a88e49a7c442ef0a4
akhil panwar <akhilpanwar180@gmail.com>
2022-11-06 17:46:41 +0530

    created front end signUp request

--- client/src/users/pages/Auth.jsx
+++ client/src/users/pages/Auth.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -15,6 +17,8 @@ import classes from "./Auth.module.css";
 
 function Auth() {
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -35,11 +39,42 @@ function Auth() {
   const authCtx = useContext(AuthContext);
   const { logIn } = authCtx;
 
-  function authSubmitHandler(event) {
+  const API_BASE = "http://localhost:5000/api/users";
+
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    logIn();
-    navigate("/", { replace: true }); // redirects to homepage and erases current page from history
-  }
+
+    if (isLoginMode) {
+    } else {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${API_BASE}/signup`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responsedata = await response.json();
+        console.log(responsedata);
+        setIsLoading(false);
+        if (!response.ok) {
+          throw new Error(responsedata.message); //error message will come from backend.
+        }
+        logIn();
+        navigate("/", { replace: true }); // redirects to homepage and erases current page from history
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setError(error.message || "Soemthing went wrong, please try again.");
+      }
+    }
+  };
 
   function switchModeHandler() {
     if (!isLoginMode) {
@@ -65,49 +100,58 @@ function Auth() {
     setIsLoginMode((prevMode) => !prevMode);
   }
 
+  function errorHandler() {
+    setError(null);
+  }
 
   return (
-    <Card className={classes.authentication}>
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && (
+    <>
+    <ErrorModal
+      error={error} onClear={errorHandler}
+    />
+      <Card className={classes.authentication}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              id="name"
+              element="input"
+              type="text"
+              label="Name"
+              onInput={inputHandler}
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name."
+            />
+          )}
           <Input
-            id="name"
+            id="email"
             element="input"
-            type="text"
-            label="Name"
+            type="email"
+            label="Email"
             onInput={inputHandler}
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a name."
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email"
           />
-        )}
-        <Input
-          id="email"
-          element="input"
-          type="email"
-          label="Email"
-          onInput={inputHandler}
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email"
-        />
-        <Input
-          id="password"
-          element="input"
-          type="password"
-          label="Password"
-          onInput={inputHandler}
-          validators={[VALIDATOR_MINLENGTH(6)]}
-          errorText="Please enter a valid password"
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? "LOGIN" : "SIGNUP"}
+          <Input
+            id="password"
+            element="input"
+            type="password"
+            label="Password"
+            onInput={inputHandler}
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Please enter a valid password"
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? "LOGIN" : "SIGNUP"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
-      </Button>
-    </Card>
+      </Card>
+    </>
   );
 }
 
